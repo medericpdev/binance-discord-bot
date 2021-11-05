f30f7ceb715585e8b15f4082bce9ce107c39439c
Vincent Hardouin <vincenthardouin@icloud.com>
2021-11-05 19:37:15 +0100

    feat: Improve config repository

--- repository/config-repository.js
+++ repository/config-repository.js
@@ -4,15 +4,17 @@ const ADAPTER_CONFIG = new FileSync('config.json');
 const config = low(ADAPTER_CONFIG);
 
 const Config = require('../models/Config');
+const Player = require('../models/Player');
 
 module.exports = {
   getConfig() {
-    const prefix = CONFIG.get('prefix').value();
-    const token = CONFIG.get('token').value();
-    const autoSave = CONFIG.get('auto_save').value();
-    const channelName = CONFIG.get('channel_name').value();
-    const showPnlHistory = CONFIG.get('show_pnl_history').value();
-    const players = CONFIG.get('players').value();
+    const prefix = config.get('prefix').value();
+    const token = config.get('token').value();
+    const autoSave = config.get('auto_save').value();
+    const channelName = config.get('channel_name').value();
+    const showPnlHistory = config.get('show_pnl_history').value();
+    let players = config.get('players').value();
+    players = players.map((player) => new Player(player));
     return new Config({
       prefix,
       token,
@@ -21,5 +23,19 @@ module.exports = {
       showPnlHistory,
       players,
     });
+  },
+
+  savePlayer(player) {
+    config.get('players').push(player).write();
+    return new Player(player);
+  },
+
+  deletePlayer(player) {
+    config.get('players').remove({ name: player.name }).write();
+  },
+
+  updatePlayer(player) {
+    config.get('players').remove({ name: player.name }).write();
+    config.get('players').push(player).write();
   },
 };
